c94a80c8b96227a81d9c97206d0c5f879f02c9d4
Oscar <oscarpete@gmail.com>
2021-03-30 15:57:04 +0200

    version 1 with only one user input

---
+++ ConsoleBank/ConsoleBank/Client.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBank
+{
+    class Client
+    {
+        private int id;
+        private string name;
+        private string dateJoined;
+
+        public Client()
+        {
+
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string DateJoined { get => dateJoined; set => dateJoined = value; }
+
+        public void clientName(string _name)
+        {
+            name = _name;
+        }
+
+    }
+}
---
+++ ConsoleBank/ConsoleBank/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using ConsoleBank;
+
+
+namespace ConsoleBank
+{
+    class Program
+    {
+
+        
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name:");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine(" ---------------------------");
+            Console.WriteLine("|     Welcome to Mybank      |");
+            Console.WriteLine("        "+userName+"       ");
+            Console.WriteLine("|   How can we help you?     |");
+            Console.WriteLine("|                            |");
+            Console.WriteLine("| 1. Balance Enquiry         |");
+            Console.WriteLine("| 2. Cash Deposit            |");
+            Console.WriteLine("| 3. Withdrawal              |");
+            Console.WriteLine("|                            |");
+            Console.WriteLine(" ---------------------------");
+
+
+
+            var account = new BankAccount(userName, 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Client} with {account.Balance} initial balance.");
+
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "friend paid me back");
+            Console.WriteLine(account.Balance);
+
+
+
+            
+
+
+
+            //Client objClient = new Client();
+            //Console.WriteLine();
+            //objClient.clientName = Console.ReadLine();
+            //string clientName = Console.ReadLine();
+            //objClient.clientName(clientName);
+            //type of action
+            //Console.WriteLine("Welcome " + clientName + " your Balance is " +  + "  would you like to: check balance, deposit or Withdrawl?");
+            //string clientOption = Console.ReadLine();
+
+
+
+
+
+
+            // BankAccount objBank = new BankAccount();
+            // objBank.makeBankAccount(clientName, 10, "saving account");
+            //Console.WriteLine("User name is: " + objBank.Client + "Your balance is " + objBank.Balance + " and is a " + objBank.Type);
+
+            // Console.WriteLine("Welcome " + clientName + " your balance is " + objBank.Balance + " and is a " + objBank.Type + " Would you like to Deposit or Withdrawl?");
+            // string clientOption = Console.ReadLine();
+
+
+            //Console.ReadLine();
+        }
+    }
+}
